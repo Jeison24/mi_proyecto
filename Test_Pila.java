b5a0b36db15443bd628882b9806717ec2c75e2f7
Jeison24 <65914948+Jeison24@users.noreply.github.com>
2020-05-25 19:16:24 +0200

    Primer git hub

---
+++ Test_Pila.java
@@ -0,0 +1,25 @@
+package mi_proyecto;
+
+import java.util.Arrays;
+
+public class Test_Pila {
+
+	public static void main(String[] args) {
+	
+		/**
+		* ESTE ES EL CAMBIO DEL ARCHIVO
+		*/
+		
+		PilaEnteros libros = new PilaEnteros (5);
+		
+		libros.push(3);
+		libros.push(6);
+		libros.push(8);
+		
+		System.out.println(libros);
+		System.out.println("========================================");
+		System.out.println();
+		System.out.println(Arrays.toString(libros.pila));
+	}
+
+}
